66b938a644230dd44dabc673b90eb44d42a2d7e4
Mitch <mitch_j88@hotmail.com>
2022-07-25 22:23:25 +0200

    Added checks on array lengths

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <v-row>
     <v-col cols="12" md="6"></v-col>
     <v-col cols="12" md="6">
+      <!-- TEMP TESTING -->
       <v-sheet class="pa-4">
         <v-btn class="mx-1" x-small @click="clearStorage">Clear storage</v-btn>
         <div class="d-flex align-center">
@@ -40,22 +41,34 @@ export default {
     clearStorage() {
       window.localStorage.clear();
     },
+
     onAddList() {
       this.addList();
     },
+
     onDeleteList() {
+      if (this.lists.length < 1) return;
+
       this.deleteList(this.lists[0].id);
     },
+
     onAddRules() {
       this.addRules(this.lists[0].id);
     },
+
     onDeleteRules() {
+      if (this.rules.length < 1) return;
+
       this.deleteRules(this.rules[0].id);
     },
+
     onAddManipulator() {
       this.addManipulator(this.rules[0].id);
     },
+
     onDeleteManipulator() {
+      if (this.manipulators.length < 1) return;
+
       this.deleteManipulator(this.manipulators[0].id);
     },
   },
